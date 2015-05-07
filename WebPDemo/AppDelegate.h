f8575c13f560585146126790e4f648feb5ec96c3
Michael <mdavidson@cala.fsu.edu>
2015-05-07 17:10:54 -0400

    Initial Commit

---
+++ WebPDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebPDemo
+//
+//  Created by Michael Davidson on 5/7/15.
+//  Copyright (c) 2015 Michael Davidson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebPDemo
+//
+//  Created by Michael Davidson on 5/7/15.
+//  Copyright (c) 2015 Michael Davidson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
